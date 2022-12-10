faf31f5c626ac3fe63860d87cd89663341058e50
KatsiarynaBarsukova <tulovo@tut.by>
2022-12-10 22:17:03 +0300

    Develop

---
+++ src/test/java/baseEntities/BaseTest.java
@@ -0,0 +1,31 @@
+package baseEntities;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import configuration.ReadProperties;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeSuite;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class BaseTest {
+
+    @BeforeSuite
+    public void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.browser = ReadProperties.browserName();
+        Configuration.baseUrl = ReadProperties.getUrl();
+        Configuration.timeout = 8000;
+        Configuration.fastSetValue = true;
+        //Configuration.assertionMode = AssertionMode.SOFT;
+        //Configuration.headless = true;
+        //Configuration.reportsFolder = "target/";
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        closeWebDriver();
+    }
+}
